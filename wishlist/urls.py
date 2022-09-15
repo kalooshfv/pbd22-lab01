45c46cd8a6edb8d2ea07b0f2b032b4fa7506c91f
Kaloosh Falito Verrell <kf.verrell@gmail.com>
2022-09-15 11:00:00 +0700

    Lab 2

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from wishlist.views import show_wishlist, show_xml, show_json, show_json_by_id
+from wishlist.views import *
 
 app_name = 'wishlist'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
+    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -27,3 +27,7 @@ def show_json_by_id(request, id):
     return HttpResponse(serializers.serialize("json", data), \
         content_type="application/json")
 
+def show_xml_by_id(request, id):
+    data = data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), \
+        content_type="application/xml")
